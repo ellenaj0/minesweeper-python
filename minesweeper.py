e96d2a0bb7a19364219076e0e79302fc8d5ec34c
xixi199 <83231427+xixi199@users.noreply.github.com>
2023-03-06 00:28:32 +0100

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -4,22 +4,33 @@ from tkmacosx import Button
 from tkinter import messagebox
 import random
 
+
 GRID_SIZE = 9
 MINES = 10
 
-window = tk.Tk()
-window.title("Minesweeper")
-# window.geometry("300x300")
 
-# Frame with the 9x9 grid
-center_frame = tk.Frame(master=window, width=300, height=300, relief=tk.RIDGE, borderwidth=10, bg="gray")
-center_frame.pack()
+class Minesweeper:
 
-# Create buttons for each cell
-for i in range(GRID_SIZE):
-    for j in range(GRID_SIZE):
-        cell = Cell(x=i, y=j, frame=center_frame)
-        cell.button.grid(row=i, column=j)
+    def __init__(self, window):
+        self.window = window
+        # Frame with the 9x9 grid
+        center_frame = tk.Frame(master=self.window, width=300, height=300, relief=tk.RIDGE, borderwidth=10, bg="gray")
+        center_frame.pack()
+        self.center_frame = center_frame
+        self.all_cells = []
+        # Create buttons for each cell
+        for i in range(GRID_SIZE):
+            for j in range(GRID_SIZE):
+                cell = Cell(x=i, y=j, frame=center_frame)
+                cell.button.grid(row=i, column=j)
+                self.all_cells.append(cell)
 
 
-window.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("Minesweeper")
+    # window.geometry("300x300")
+    minesweeper = Minesweeper(root)
+
+    root.mainloop()
+
